f6199d4b825b6c92f9d45a27d0938d74edb76642
brendanc <brendan@devspade.com>
2025-12-06 11:35:08 -0500

    task 4 partial

--- src/main/kotlin/TaskResolver.kt
+++ src/main/kotlin/TaskResolver.kt
@@ -4,6 +4,7 @@ import main.kotlin.tasks.BaseTask
 import main.kotlin.tasks.Task1
 import main.kotlin.tasks.Task2
 import main.kotlin.tasks.Task3
+import main.kotlin.tasks.Task4
 
 class TaskResolver {
     fun resolveTask(taskNumber: Int) : BaseTask
@@ -12,6 +13,7 @@ class TaskResolver {
             1 -> return Task1()
             2 -> return Task2()
             3 -> return Task3()
+            4 -> return Task4()
             else -> throw IllegalArgumentException("Task $taskNumber not yet supported")
         }
     }
